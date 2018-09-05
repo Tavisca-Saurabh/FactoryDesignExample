e5a749259334b52fbee2a6bc7af57f6707801905
Saurabh Manchanda <smanchanda@tavisca.com>
2018-09-05 14:24:30 +0530

    Factory Design

---
+++ FactoryDesignExample/FactoryDesignExample/AirProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignExample
+{
+    class AirProduct:IProduct
+    {
+        public string GetTypeOfProduct()
+        {
+            return "Car";
+        }
+        public void Book()
+        {
+            Console.WriteLine("Air - Book");
+        }
+        public void Save()
+        {
+            Console.WriteLine("Air - Save");
+        }
+    }
+}
